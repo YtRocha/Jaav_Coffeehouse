0e12afb00bf9647c40422cf292d8aad35ced2cbe
YtRocha <ytallo4gamer@gmail.com>
2021-11-15 15:05:44 -0300

    Adiçao da opçao append no FileWriter

--- src/Utils/Txt/EscreverArquivo.java
+++ src/Utils/Txt/EscreverArquivo.java
@@ -1,14 +1,26 @@
 package Utils.Txt;
 
+
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 
 public class EscreverArquivo {
+
+    private String caminho;
+
+    private String texto;
+
+
+    public EscreverArquivo(String caminho, String texto) {
+        this.caminho = caminho;
+        this.texto = texto;
+    }
+
     
-    public static boolean escreverArquivo(String caminho, String texto){
+    public boolean escreverArquivo(){
         try{
-            FileWriter arquivo = new FileWriter(caminho);
+            FileWriter arquivo = new FileWriter(caminho, true);
             PrintWriter escreverArquivo = new PrintWriter(arquivo);
             escreverArquivo.println(texto);
             escreverArquivo.close();
