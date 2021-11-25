684314ddcceabf84da95ec5177040c52e227f381
Felipe S <rfelipe273@gmail.com>
2021-11-25 13:26:45 -0300

    adicionada lista das vendas à aba vendas/listar.

--- src/Views/Vendas/ListaVendas.java
+++ src/Views/Vendas/ListaVendas.java
@@ -2,15 +2,22 @@ package Views.Vendas;
 
 import javax.swing.*;
 
+import Models.Vendas.LerVendas;
+
 public class ListaVendas extends AbaVendas {
-    JLabel labelTitulo = new JLabel("Lista de Vendas");
+    Object [][] vendas = new LerVendas().dadosVendas();
 
     public ListaVendas() {
         inicializar();
     }
 
     public void inicializar(){
-        painel.add(new JLabel("Lista de vendas"));
-    }
 
+        String [] colunas = { "Produto", "Preço", "Quantidade" };
+
+        JTable tabela = new JTable(vendas, colunas);
+        JScrollPane scroll = new JScrollPane(tabela);
+        
+        painel.add(scroll);
+    }
 }
