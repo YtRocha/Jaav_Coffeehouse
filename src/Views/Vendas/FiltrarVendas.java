e8821f79624216a3439e8952174e6a6b2070b3e7
Felipe S <rfelipe273@gmail.com>
2021-11-25 15:06:54 -0300

    adicionado campo para filtrar vendas por CPF.

--- src/Views/Vendas/FiltrarVendas.java
+++ src/Views/Vendas/FiltrarVendas.java
@@ -1,24 +1,64 @@
 package Views.Vendas;
 
 import javax.swing.*;
+import java.awt.BorderLayout;
 
 import Models.Vendas.LerVendas;
+import Views.Formulario;
 
-public class FiltrarVendas extends AbaVendas {
-    Object [][] vendas = new LerVendas().dadosVendasIdentificadas();
+public class FiltrarVendas extends AbaVendas implements Formulario {
+    Object [][] vendas = new LerVendas().dadosVendasIdentificadas(null);
+
+    JLabel lblFiltrarVendas = new JLabel("Digite um CPF para filtrar:");
+    JTextField txtFiltrarVendas = new JTextField(10);
+    JButton btnFiltrarVendas = new JButton("Filtrar");
+
+    JPanel pnlPesquisa = new JPanel( new BorderLayout() );
+
+    JPanel pnlTabela = new JPanel();
 
     public FiltrarVendas() {
         inicializar();
+        eventos();
     }
 
     public void inicializar(){
+        painel.setLayout(new BorderLayout());
+        painel.add(this.getPnlPesquisa(), BorderLayout.PAGE_START);
+        painel.add(this.getPnlTabela(), BorderLayout.CENTER);
+    }
+    
+    public JPanel getPnlPesquisa(){
+        pnlPesquisa.add(lblFiltrarVendas, BorderLayout.PAGE_START);
+        pnlPesquisa.add(txtFiltrarVendas, BorderLayout.CENTER);
+        pnlPesquisa.add(btnFiltrarVendas, BorderLayout.PAGE_END);
+
+        return pnlPesquisa;
+    }
+
+    public JPanel getPnlTabela(){
+        pnlTabela.setLayout(new BorderLayout());
 
         String [] colunas = { "Produto", "Preço", "Quantidade", "Comprador" };
-        
+
         JTable tabela = new JTable(vendas, colunas);
         JScrollPane scroll = new JScrollPane(tabela);
-        painel.add(scroll);
 
+        pnlTabela.add(scroll);
+
+        return pnlTabela;
     }
+
+    public void eventos(){
+        btnFiltrarVendas.addActionListener(e -> {
+            String cpf = txtFiltrarVendas.getText().trim();
+            vendas = new LerVendas().dadosVendasIdentificadas(!cpf.equals("") ? cpf : null);
+            pnlTabela.removeAll();
+            inicializar();
+            eventos();
+        });
+    }
+
+
 
 }
