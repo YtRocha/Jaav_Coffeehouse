6b3cebc524a4270c87e8096f0e1a9d1371818b52
Felipe S <rfelipe273@gmail.com>
2021-11-30 12:32:35 -0300

    adicionado formulario para cadastro de clientes.

--- src/Views/Clientes/CadastrarClientes.java
+++ src/Views/Clientes/CadastrarClientes.java
@@ -1,26 +1,67 @@
 package Views.Clientes;
 
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import Views.Formulario;
 
-public class CadastrarClientes extends AbaClientes implements Formulario {
-    
-    JLabel labelTitulo = new JLabel("Cadastro de Clientes");
-    JTextField textFieldNome = new JTextField();
-    
+public class CadastrarClientes extends AbaClientes implements Formulario{
+    JLabel labelNome = new JLabel("Digite o nome do cliente:");
+    JTextField campoNome = new JTextField(10);
+
+    JLabel labelCPF = new JLabel("Digite CPF do cliente:");
+    JTextField campoCPF = new JTextField(10);
+
+    JButton botaoCadastrar = new JButton("Cadastrar");
+
     public CadastrarClientes() {
         inicializar();
+        eventos();
     }
 
-    public void inicializar(){
-        painel.add(labelTitulo);
+    public void inicializar() {
+        painel.setLayout(new BorderLayout());
+
+        botaoCadastrar.setPreferredSize(new Dimension(120, 30));
+        botaoCadastrar.setFont(new Font("Arial", Font.BOLD, 14));
+        botaoCadastrar.setFocusPainted(false);
+        botaoCadastrar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        JPanel painelCadastro = new JPanel(new GridLayout(12, 2));
+        JPanel leftSpace = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel rightSpace = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JPanel rodape = new JPanel();
+
+        painelCadastro.add(labelNome);
+        painelCadastro.add(campoNome);
+        painelCadastro.add(labelCPF);
+        painelCadastro.add(campoCPF);
+
+        rodape.add(botaoCadastrar);
+        painel.add(leftSpace, BorderLayout.LINE_START);
+        painel.add(rightSpace, BorderLayout.LINE_END);
+        painel.add(painelCadastro, BorderLayout.CENTER);
+
+        painel.add(rodape, BorderLayout.PAGE_END);
     }
 
-    @Override
     public void eventos() {
-        // TODO Implementar eventos
-        
+        botaoCadastrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Implementar eventos para cadastro de clientes
+            }
+        });
     }
-
 }
