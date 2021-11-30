de648b4d08dbd277e0bc472dc4003a8c7eccdc39
Felipe S <rfelipe273@gmail.com>
2021-11-30 13:50:43 -0300

    adicionado modelo para lista de clientes excluidos.

---
+++ src/Models/Clientes/LerClientesExcluidos.java
@@ -0,0 +1,25 @@
+package Models.Clientes;
+
+import java.util.ArrayList;
+
+import Utils.Txt.LerArquivo;
+
+public class LerClientesExcluidos {
+    LerArquivo clientes = new LerArquivo("src/Database/clientes_excluidos.txt");
+
+    public String[][] dadosClientes() {
+        String[] clientes = this.clientes.lerArquivo().split("\n");
+
+        ArrayList<String[]> dados = new ArrayList<String[]>();
+
+        String[][] dados_array = new String[dados.size()][];
+
+        for (String cliente : clientes) {
+            String[] dados_cliente = cliente.split("\\|");
+            dados.add(dados_cliente);
+        }
+        
+        dados_array = dados.toArray(dados_array);
+        return dados_array;
+    }
+}
