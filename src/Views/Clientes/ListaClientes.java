4db2011a1a1ca96fc26fa25acddb61f9a33bcfea
Felipe S <rfelipe273@gmail.com>
2021-11-30 13:46:13 -0300

    removido mock de dados em clientes.

--- src/Views/Clientes/ListaClientes.java
+++ src/Views/Clientes/ListaClientes.java
@@ -2,21 +2,9 @@ package Views.Clientes;
 
 import javax.swing.*;
 
-import Models.Vendas.LerVendas;
+import Models.Clientes.LerClientes;
 public class ListaClientes extends AbaClientes {
-    // TODO: implementar model de dados.
-    String[][] clientes_mock = {
-            {"1", "João", "123456789", ""},
-            {"2", "Maria", "987654321", ""},
-            {"3", "Pedro", "123456789", ""},
-            {"4", "Ana", "987654321", ""},
-            {"5", "José", "123456789", ""},
-            {"6", "Paula", "987654321", ""},
-            {"7", "José", "123456789", ""},
-            {"8", "Paula", "987654321", ""},
-            {"9", "José", "123456789", ""},
-            {"10", "Paula", "987654321", ""},
-            {"11", "José", "123456789", ""}};
+    String[][] clientes = new LerClientes().dadosClientes();
 
     public ListaClientes() {
         inicializar();
@@ -26,7 +14,7 @@ public class ListaClientes extends AbaClientes {
         painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
         String [] colunas = { "ID", "Nome", "CPF" };
 
-        JTable tabela = new JTable(clientes_mock, colunas);
+        JTable tabela = new JTable(clientes, colunas);
         JScrollPane scroll = new JScrollPane(tabela);
         
         painel.add(scroll);
