e1febeb7e9c33373cb9325cf549d523f26de8575
Felipe S <rfelipe273@gmail.com>
2021-11-30 13:45:52 -0300

    removido mock de dados em clientes excluidos.

--- src/Views/Clientes/ListaClientesExcluidos.java
+++ src/Views/Clientes/ListaClientesExcluidos.java
@@ -2,20 +2,10 @@ package Views.Clientes;
 
 import javax.swing.*;
 
+import Models.Clientes.LerClientesExcluidos;
+
 public class ListaClientesExcluidos extends AbaClientes {
-    // TODO: implementar model de dados.
-    String[][] clientes_mock = {
-            {"1", "João", "123456789", ""},
-            {"2", "Maria", "987654321", ""},
-            {"3", "Pedro", "123456789", ""},
-            {"4", "Ana", "987654321", ""},
-            {"5", "José", "123456789", ""},
-            {"6", "Paula", "987654321", ""},
-            {"7", "José", "123456789", ""},
-            {"8", "Paula", "987654321", ""},
-            {"9", "José", "123456789", ""},
-            {"10", "Paula", "987654321", ""},
-            {"11", "José", "123456789", ""}};
+    String[][] clientes_excluidos = new LerClientes().dadosClientes();
 
     public ListaClientesExcluidos() {
         inicializar();
@@ -25,7 +15,7 @@ public class ListaClientesExcluidos extends AbaClientes {
         painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
         String [] colunas = { "ID", "Nome", "CPF", "Data de exlusão" };
 
-        JTable tabela = new JTable(clientes_mock, colunas);
+        JTable tabela = new JTable(clientes_excluidos, colunas);
         JScrollPane scroll = new JScrollPane(tabela);
         
         painel.add(scroll);
