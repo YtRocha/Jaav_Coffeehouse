d7cf7cf12e1bb4d41f8eb25091aa04fe460c0120
YtRocha <ytallo4gamer@gmail.com>
2021-12-04 16:18:12 -0300

    criaçao da ExcluiProdutos, correçoes nos models de produtos e na EscreverJSON

--- src/Utils/JSON/EscreverJSON.java
+++ src/Utils/JSON/EscreverJSON.java
@@ -32,7 +32,7 @@ public class EscreverJSON {
             json.close();
             return true;
         } catch (IOException erro) {
-            System.out.println("Erro ao escrever o arquivo.");
+            System.out.println("Erro ao escrever o arquivo");
             System.out.println(erro.getMessage());
             return false;
         }
