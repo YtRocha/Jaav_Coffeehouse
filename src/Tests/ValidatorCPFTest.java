ea0e5fc663e07de6eda92639f24851180187b440
Felipe S <rfelipe273@gmail.com>
2021-12-04 17:28:53 -0300

    adicionado alguns testes sobre validador de CPF.

---
+++ src/Tests/ValidatorCPFTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import org.junit.Test;
+import Utils.Validators.*;
+
+public class ValidatorCPFTest {
+    @Test
+    public void isValidCpf_should_return_false_if_invalid_cpf_is_provided() {
+        assert !Validator.isCpfValid("12345678901");
+        assert !Validator.isCpfValid("12345678123");
+    }
+
+    public void isValidCpf_should_return_true_if_invalid_cpf_is_provided() {
+        // CPF gerado.
+        assert Validator.isCpfValid("63613186055");
+    }
+}
