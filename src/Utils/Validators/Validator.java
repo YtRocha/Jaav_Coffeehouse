53f1715adb623f38f98d386f3fc738a5363b2a36
Felipe S <rfelipe273@gmail.com>
2021-12-05 22:25:32 -0300

    remove metodo main de teste de Validator.

--- src/Utils/Validators/Validator.java
+++ src/Utils/Validators/Validator.java
@@ -1,6 +1,5 @@
 package Utils.Validators;
 
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -13,23 +12,17 @@ public class Validator {
     public static boolean isDateValid(String strDate) {
         String dateFormat = "dd/MM/uuuu";
 
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormat).withResolverStyle(ResolverStyle.STRICT);
-        try{
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormat)
+                .withResolverStyle(ResolverStyle.STRICT);
+        try {
             LocalDate date = LocalDate.parse(strDate, dateTimeFormatter);
             return true;
-        } catch(DateTimeParseException erro){
+        } catch (DateTimeParseException erro) {
             return false;
         }
     }
 
     public static boolean isCpfValid(String cpf) {
         return ValidatorCPF.isCpf(cpf);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(isCpfValid("08739394506"));
-        System.out.println(isDateValid("30/04/2021"));
-        System.out.println(isDateValid("31/04/2021"));
-        System.out.println(isDateValid("300/04/2021"));
     }
 }
