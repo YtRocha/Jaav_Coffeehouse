776c46fcf6f6ae694b1a3f5fe994ee39d6928b64
Felipe S <rfelipe273@gmail.com>
2021-12-06 22:09:42 -0300

    adicionado botão de atualizar  lista de clientes.

--- src/Views/Clientes/ListaClientes.java
+++ src/Views/Clientes/ListaClientes.java
@@ -7,17 +7,34 @@ import Models.Clientes.LerClientes;
 public class ListaClientes extends AbaClientes {
     String[][] clientes = new LerClientes().dadosClientes();
 
+    JButton btnAtualizar = new JButton("Atualizar");
+
     public ListaClientes() {
         inicializar();
     }
 
     public void inicializar() {
         painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
+
+        JPanel painelBotoes = new JPanel();
+        painelBotoes.setLayout(new BoxLayout(painelBotoes, BoxLayout.X_AXIS));
+        painelBotoes.add(btnAtualizar);
+
+        painel.add(painelBotoes);
+
         String[] colunas = { "Nome", "CPF" };
 
         JTable tabela = new JTable(clientes, colunas);
         JScrollPane scroll = new JScrollPane(tabela);
 
         painel.add(scroll);
+    }
+
+    public void eventos() {
+        btnAtualizar.addActionListener(e -> {
+            clientes = new LerClientes().dadosClientes();
+            painel.removeAll();
+            inicializar();
+        });
     }
 }
--- src/Views/Clientes/ListaClientesExcluidos.java
+++ src/Views/Clientes/ListaClientesExcluidos.java
@@ -6,6 +6,7 @@ import Models.Clientes.LerClientesExcluidos;
 
 public class ListaClientesExcluidos extends AbaClientes {
     String[][] clientes_excluidos = new LerClientesExcluidos().dadosClientes();
+    JButton btnAtualizar = new JButton("Atualizar");
 
     public ListaClientesExcluidos() {
         inicializar();
@@ -13,11 +14,24 @@ public class ListaClientesExcluidos extends AbaClientes {
 
     public void inicializar() {
         painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
+        JPanel painelBotoes = new JPanel();
+        painelBotoes.setLayout(new BoxLayout(painelBotoes, BoxLayout.X_AXIS));
+        painelBotoes.add(btnAtualizar);
+
+        painel.add(painelBotoes);
         String[] colunas = { "Nome", "CPF", "Data de exlusão" };
 
         JTable tabela = new JTable(clientes_excluidos, colunas);
         JScrollPane scroll = new JScrollPane(tabela);
 
         painel.add(scroll);
+    }
+
+    public void eventos() {
+        btnAtualizar.addActionListener(e -> {
+            clientes_excluidos = new LerClientesExcluidos().dadosClientes();
+            painel.removeAll();
+            inicializar();
+        });
     }
 }
